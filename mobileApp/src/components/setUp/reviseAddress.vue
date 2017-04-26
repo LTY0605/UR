65787a58a60146af9a0f48c2ab98cd582d39a7f1
xianyuchan <xianyuchan@foreveross.com>
2017-04-26 11:10:57 +0800

    样式修改

--- mobileApp/src/components/setUp/reviseAddress.vue
+++ mobileApp/src/components/setUp/reviseAddress.vue
@@ -45,85 +45,154 @@
     }
 </script>
 <style lang="less" rel="stylesheet/less">
-    .vux-header .vux-header-left .left-arrow:before{
+    .dialog-demo {
+        font-size: .9rem;
+        .dialog-title {
+            font-size: 1rem;
+            height: 2.2rem;
+            line-height: 2.2rem;
+        }
+        .dialog-contain {
+            width: 100%;
+            padding: .5rem 0 1rem 0;
+            border-bottom: 1px solid #ddd;
+            color: #999999;
+        }
+        .vux-close {
+            height: 2rem;
+            line-height: 2rem;
+            color: #0BB20C;
+            background: none;
+        }
+    }
+
+    .operate {
+        height: 2rem;
+        line-height: 2rem;
+        text-align: center;
+        font-size: .75rem;
+        background: #AB9236;
+        color: #fff;
+        margin-top: 1.25rem;
+        border-radius: .2rem;
+    }
+
+    .vux-header .vux-header-left .left-arrow:before {
         border: 1px solid #FFFFFF;
         border-width: 1px 0 0 1px;
     }
-    .vux-header-right a{
+
+    .vux-header-right a {
         color: #FFFFFF !important;
     }
+
     .page_reviseAddress {
-    .weui-cells .vux-no-group-title:after{
-        border: none !important;
-    }
-    .vux-header {
-        background-color: #AB9236 !important;
-    }
-    .vux-header .vux-header-title, .vux-header h1 {
-        font-size: .85rem;
-    }
-    .weui-btn_default{
-        background: #AB9236 !important;
-        color: #FFFFFF !important;
-        height: 2rem;
-    }
-    .address-con{
-        padding: 0 1rem;
-        border-bottom: .35rem solid #E5E5E5;
-    .weui-cells{
-        margin-top: .25rem !important;
-    }
-    .weui-cells:before{
-        border: none;
-    }
-    .weui-cells:after{
-        border: 3px solid #FFFFFF;
-    }
-    .weui-cell{
-        padding: .5rem 0 !important;
-        border-bottom: 1px solid #D2D2D2;
-    }
-    .weui-cell:before{
-        border: none;
-    }
-    .weui-label{
-        font-size: .75rem;
-        color: #333333;
-        height: .8rem;
-        line-height: .8rem;
-        margin-right: .5rem;
-    }
-    .consignee{
-    .weui-label{
-        border-right: 1px solid #d2d2d2;
-    }
-    }
-    .address-select{
-    .vux-cell-box:before{
-        border: none;
-    }
-    .weui-cell{
-        padding: 1rem 0 !important;
-    }
-    .vux-popup-picker-select{
-        text-align: right !important;
-        height: .85rem;
-        line-height: .85rem;
-    }
-    .vux-popup-picker-select span{
-        font-size: .75rem;
-        color: #999999;
-    }
-    }
-    .address-text{
-        padding: 1rem 0 .5rem 0 !important;
-    .weui-textarea{
-        font-size: .75rem;
-        font-family: inherit;
-        height: 5.2rem;
-    }
-    }
-    }
+        .text {
+            display: block;
+            height: 2rem;
+            line-height: 2rem;
+        }
+        input[type="checkbox"] {
+            display: none;
+        }
+        input[type="checkbox"] + span {
+            width: .85rem;
+            height: .85rem;
+            line-height: .85rem;
+            background: url(../../assets/images/circle.png) left center no-repeat;
+            background-size: .85rem .85rem;
+            padding-left: 1.3rem;
+            font-size: .75rem;
+        }
+        input[type="checkbox"]:checked + span {
+            background: url(../../assets/images/circle_a.png) left center no-repeat;
+            background-size: .85rem .85rem;
+        }
+        .weui-cells .vux-no-group-title:after {
+            border: none !important;
+        }
+        .vux-header {
+            background-color: #AB9236 !important;
+        }
+        .vux-header .vux-header-title, .vux-header h1 {
+            font-size: .85rem;
+        }
+        .weui-btn_default {
+            background: #AB9236 !important;
+            color: #FFFFFF !important;
+            height: 2rem;
+        }
+        .address-con {
+            padding: 0 1rem;
+            border-bottom: .35rem solid #E5E5E5;
+            .weui-cells {
+                margin-top: .25rem !important;
+            }
+            .weui-cells:before {
+                border: none;
+            }
+            .weui-cells:after {
+                border: 3px solid #FFFFFF;
+            }
+            .weui-cell {
+                padding: .8rem 0 !important;
+                border-bottom: 1px solid #D2D2D2;
+                font-size: .75rem;
+            }
+            .weui-cell:before {
+                border: none;
+            }
+            .weui-label {
+                font-size: .75rem;
+                color: #333333;
+                height: .8rem;
+                line-height: .8rem;
+            }
+            .address-select {
+                .vux-cell-box:before {
+                    border: none;
+                }
+                .weui-cell {
+                    padding: 1rem 0 !important;
+                }
+                .vux-popup-picker-select {
+                    text-align: right !important;
+                    height: .85rem;
+                    line-height: .85rem;
+                }
+                .vux-popup-picker-select span {
+                    font-size: .75rem;
+                    color: #999999;
+                    display: inline-block;
+                }
+            }
+            .address-text {
+                padding: 1rem 0 .5rem 0 !important;
+                .weui-textarea {
+                    font-size: .75rem;
+                    font-family: inherit;
+                    height: 5.2rem;
+                }
+            }
+        }
+        .checkAddress {
+            .weui-cell {
+                padding: .4rem 0 1rem 0;
+            }
+            .weui-cells:after {
+                border: none !important;
+            }
+            i {
+                transform: scale(0.7);
+            }
+            .weui-cells_checkbox > label > * {
+                font-size: .6rem;
+                margin-left: -.3rem;
+            }
+            .weui-cell__bd > p {
+                margin-bottom: .2rem;
+            }
+        }
         .address-foot{
             padding: 0 1rem;
             .weui-cells:before{
@@ -140,4 +209,6 @@
             }
         }
     }
+
+
 </style>
