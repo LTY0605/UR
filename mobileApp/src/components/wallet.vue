6a04c9e2fd26582224a6938a0e01b21bc701d2e7
xianyuchan <xianyuchan@foreveross.com>
2017-04-27 14:23:00 +0800

    回滚代码

--- mobileApp/src/components/wallet.vue
+++ mobileApp/src/components/wallet.vue
@@ -1,12 +1,21 @@
 <template>
     <div class="page_wallet">
         <x-header :left-options="{backText: ''}">我的钱包</x-header>
-        <div class="tabTitle">
-            <span :class="{active:titleTab==index}" v-for="(item, index) in titleList" @click="titleTab = index">{{item.name}}</span>
+        <div class="tabDown">
+            <cell style="background: #f2edda" is-link :border-intent="false" :arrow-direction="show ? 'up' : 'down'" @click.native="show = !show">
+                <p>礼品卡</p>
+            </cell>
+
+            <div class="slide" :class="show?'animate':''">
+                <div class="slide-div">
+                    <span :class="{active:titleTab==index}" v-for="(item, index) in titleList" @click="titleTab = index;show = false">{{item.name}}</span>
+                </div>
+            </div>
         </div>
+
         <div class="tabContain">
             <div class="tabItem tab-swiper" v-if="titleTab==0">
-                <giftCoupon></giftCoupon>
+                <card></card>
             </div>
             <div class="tabItem" v-if="titleTab==1">
                 <coupon></coupon>
@@ -20,18 +29,20 @@
 <script>
     import Integral from './wallet/integral.vue'
     import Coupon from './wallet/coupon.vue'
-    import giftCoupon from './wallet/giftCoupon.vue'
-    import {XHeader, Scroller} from 'vux'
+    import Card from './wallet/card.vue'
+    import {XHeader, Scroller, Cell} from 'vux'
     export default {
         components: {
-            XHeader, Scroller, Integral, Coupon,giftCoupon
+            XHeader, Scroller, Integral, Coupon, Card, Cell
         },
         data () {
             return {
+                show: false,
                 titleTab: 0,
                 list2: ['礼品卡', '优惠券', '积分'],
                 index: 1,
                 time: '',
+                tabTitle: '礼品卡',
                 titleList: [
                     {
                         code: 0,
@@ -59,6 +70,64 @@
 </script>
 <style lang="less" rel="stylesheet/less">
     .page_wallet {
+        .tabDown{
+            .vux-label{
+                text-align: center;
+            }
+            .weui-cell__ft{
+                padding: 0 !important;
+                width: 100%;
+                text-align: center;
+                p{
+                    font-size: .75rem !important;
+                    color: #ab9236 !important;
+                }
+            }
+            .weui-cell__ft:after{
+                border-color: #ab9236;
+            }
+            .slide {
+                padding: 0 1rem;
+                width: 100%;
+                overflow: hidden;
+                max-height: 0;
+                position: absolute;
+                z-index: 1000;
+                background: #FFFFFF;
+                transition: max-height .5s cubic-bezier(0, 1, 0, 1) -.1s;
+                .slide-div{
+                    padding-top: 1rem;
+                    padding-bottom: .5rem;
+                    font-size: 0;
+                    display: flex;
+                    flex-direction: row;
+                    flex-wrap: wrap;
+                    justify-content: space-between;
+                    span{
+                        display: inline-block;
+                        width: 4.2rem;
+                        height: 1.6rem;
+                        line-height: 1.5rem;
+                        text-align: center;
+                        border-radius: .15rem;
+                        font-size: .75rem;
+                        margin: 0 0 .5rem 0;
+                        border: 1px solid #CDBE86;
+                        background: #FFFFFF;
+                        color: #999999;
+                    }
+                    span.active {
+                        background: #CDBE86;
+                        color: #fff;
+                    }
+                }
+        }
+    .animate {
+        max-height: 9999px;
+        transition-timing-function: cubic-bezier(0.5, 0, 1, 0);
+        transition-delay: 0s;
+    }
+        }
         .vux-header {
             background-color: #AB9236 !important;
         }
