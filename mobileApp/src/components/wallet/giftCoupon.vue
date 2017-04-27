f4a47d0bb8b18e0b9d6a430ebaa010ed4198a3e7
可可乐 <weichunyi@foreveross.com>
2017-04-27 14:52:19 +0800

    礼品卡

--- mobileApp/src/components/wallet/giftCoupon.vue
+++ mobileApp/src/components/wallet/giftCoupon.vue
@@ -48,11 +48,48 @@
             </div>
          </div>
       </div>
+      <div  class="page_pay">
+         <!--<x-dialog v-model="showNoScroll"  class="dialog-demo" :scroll="false">-->
+            <div  class="payCode">
+               <div class="tabTitle">
+                  <span :class="{active:titleTab==index}" v-for="(item, index) in titleList" @click="titleTab = index">{{item.name}}</span>
+               </div>
+               <div class="tabContain">
+                  <div class="tabItem tab-swiper" v-if="titleTab==0">
+                     <div >
+                        <!--<x-dialog v-model="showNoScroll"  class="dialog-demo" :scroll="false">-->
+                           <div  class="scanPay">
+                              <p class="scanPay-title">京东储值卡</p>
+                              <div class="pay-box">
+                                 <img class="scanPay-img1" src="../../assets/money_code.png" alt=""/>
+                              </div>
+                              <p class="scanPay-number">URV00000018</p>
+                              <div class="pay-box">
+                                 <img class="scanPay-img2" src="../../assets/money_code.png" alt=""/>
+                              </div>
+                              <p class="scanPay-massege">如不能扫描请刮开密码</p>
+                              <div class="scanPay-close"></div>
+                           </div>
+                        <!--</x-dialog>-->
+                     </div>
+                  </div>
+                  <div class="tabItem" v-if="titleTab==1">
+                     <img class="pay-img" src="../../assets/money_code2.png" alt=""/>
+                     <p class="pay-text"><a href="#">返 回</a></p>
+                  </div>
+               </div>
+            </div>
+         <!--</x-dialog>-->
+      </div>
    </div>
 </template>
 
 <script>
+    import {XHeader, Scroller} from 'vux'
    export default{
+       components: {
+           XHeader, Scroller
+       },
        data(){
            return{
                massege1List:[{
@@ -65,13 +102,30 @@
                    type:'UR投资人专属卡',
                    number:'URV00000018',
                    money:'￥10000.00'
-               }]
+               }],
+               titleTab: 0,
+               list2: ['扫扫支付', '付款码'],
+               index: 1,
+               time: '',
+               titleList: [
+                   {
+                       code: 0,
+                       name: '扫扫支付'
+                   },
+                   {
+                       code: 1,
+                       name: '付款码'
+                   },
+
+               ]
+
            }
        },
        mounted(){
        },
        created(){
-       }
+       },
+       computed: {}
    }
 
 </script>
@@ -96,6 +150,9 @@
    }
    .list {
       list-style: none;
+      font-family: PingFang-SC-Medium;
+   }
+
    .Type {
       font-size: .9rem;
    }
@@ -105,7 +162,7 @@
    .money {
       font-size: .9rem;
    }
-   }
+
    .picture {
       width: 1.3rem;
       height: 1.3rem;
@@ -115,21 +172,120 @@
    }
    .buttonBox {
       font-size: 0;
-      display: flex;
-      justify-content: space-between;
+      /*display: flex;*/
+      /*justify-content: space-between;*/
+      margin: auto 0;
       height: auto;
    }
-   .Box1 {
+   .Box1 ,.Box2 ,.Box3{
       background-color: #CDBE86;
       color: #473A0C;
-   }
-   .Box2 {
-      background-color: #CDBE86;
-      color: #473A0C;
+      margin: 0rem;
+      padding: 0rem;
+      width: 27.7%;
+      height: 1.6rem;
+      margin-right:8.45%;
    }
    .Box3 {
-      background-color: #CDBE86;
-      color: #473A0C;
+      margin-right: 0;
+      padding: 0rem;
+   }
+   .page_pay{
+      display: flex;
+      justify-content: center;
+
+   }
+   .payCode{
+      width: 12.55rem;
+      height: auto;
+      font-family:PingFang-SC-Medium;
+      margin: 0;
+      padding: 0;
+
+   }
+   .pay-text{
+      text-align: center;
+      font-size: .6rem;
+      margin-bottom: .9rem;
+      a{
+         list-style: none;
+         color: #F68B79;
+         text-decoration: underline;
+
+      }
+   }
+
+   .tabTitle{
+      width: 100%;
+      height: auto;
+      font-size: 0;
+      span{
+         font-size: .75rem;
+         display: inline-block;
+         width: 50%;
+         background: #ffffff;
+         color: #000000;
+         border-left: 1px solid #CDBE86;
+         height: 2.5rem;
+         text-align: center;
+         padding-top: .25rem;
+
+      }
+
+   }
+   .pay-img{
+      width: 7.5rem;
+      height: 7.5rem;
+      margin:2.75rem 2.5rem 1.2rem ;
+
+
+   }
+   .scanPay{
+      width: 12.5rem;
+      height:15.1rem;
+      position: relative;
+      background: white;
+      font-size: 0;
+
+   .scanPay-title{
+      font-size: .7rem;
+      text-align: center;
+      color: #000000;
+      margin: .5rem 4.5rem;
+
+   }
+   .pay-box{
+      display: flex;
+      justify-content: center;
+   }
+   .scanPay-massege{
+      width:auto;
+      font-size: .6rem;
+      text-align: center;
+      color: #FF0018;
+   }
+   .scanPay-img1,.scanPay-img2{
+      width: 7.85rem;
+      height: 2.2rem;
+
+   }
+   .scanPay-img2{
+      margin: .6rem auto;
+   }
+   .scanPay-number{
+      text-align: center;
+      font-size: .6rem;
+      padding: .2rem 0rem .5rem;
+      border-bottom: 1px solid #CDBE86;
+   }
+   .scanPay-close{
+      position: absolute;
+      width: .8rem;
+      height: .8rem;
+      background-size: 100%;
+      top: -.15rem;
+      right: .6rem;
+   }
    }
    .gift2 {
       margin: 1.2rem 1rem;
@@ -171,7 +327,9 @@
       background-color:#CDBE86;
       color:#473A0C;
       margin: 0rem;
+      padding: 0rem;
       width: 27.7%;
+      height: 1.6rem;
       margin-right:8.45%;
    }
    .Box6{
@@ -180,6 +338,7 @@
       margin: 0rem;
       width: 27.7%;
       margin-right: 0;
+      padding: 0rem;
    }
    .btnUp{
       margin: .5rem 0;
