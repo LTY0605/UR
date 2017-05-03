650ab45181573ce69eceb0fffc77a4ee95f97f7a
litstars <1048782867@qq.com>
2017-05-03 16:09:53 +0800

    主页修改

--- mobileApp/src/components/index.vue
+++ mobileApp/src/components/index.vue
@@ -151,12 +151,6 @@
                 }
                 this.customerName = window.localStorage.getItem('customerName');
                 this.cardcode = window.localStorage.getItem('cardcode');
-                //检测是否登录
-                if(this.customerName == '' || this.customerName == undefined || this.customerName == null){
-                    this.$router.push({
-                        name: 'login'
-                    })
-                }
             },
             show() {
                 this.showNoScroll = true;
