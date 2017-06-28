099bd1fe7ff90c6bf252a22164c46e1efe9ed4f1
梁天佑 <liangtianyou@foreveross.com>
2017-06-28 11:01:43 +0800

    格式对齐

--- mobileApp/src/components/quest.vue
+++ mobileApp/src/components/quest.vue
@@ -6,7 +6,7 @@
             <div class="con-text">
                 <p>尊敬的{{customerName}}会员：</p>
                 <p>UR致力于为您提供<br/>高品质的时尚产品和优质服务！<br>
-                为了提高您的购物体验，<br>请您提供宝贵的意见，<br>完成问卷即可获赠<span class="con-integral">300会员积分</span>，<br>谢谢！</p>
+                    为了提高您的购物体验，<br>请您提供宝贵的意见，<br>完成问卷即可获赠<span class="con-integral">300会员积分</span>，<br>谢谢！</p>
             </div>
             <span @click="goLinkQuest" class="con-btn">开始问卷</span>
         </div>
@@ -16,43 +16,54 @@
 </template>
 
 <script>
-    import { surveyServices } from '../services/quest.js'
+    import {surveyServices} from '../services/quest.js'
     import {XHeader, Toast} from 'vux'
     export default{
-        components:{
+        components: {
             XHeader, Toast
         },
         data(){
-            return{
-                customerName:'',
+            return {
+                customerName: '',
                 showNoScroll2: false,
-                surveyType:'02',
-                surveyCode:'wqdc',
+                surveyType: '02',
+                surveyCode: 'wqdc',
                 warnText: ''
             }
         },
-        methods:{
+        methods: {
             questName(){
                 this.customerName = window.localStorage.getItem('customerName');
             },
             goLinkQuest(){
-                if(window.navigator.onLine==true){
+                let _this = this;
+                if (window.navigator.onLine == true) {
                     surveyServices().save({
                         cardcode: window.localStorage.getItem("cardcode"),
                         surveyType: this.surveyType,
                         surveyCode: this.surveyCode
-                    }).then(res=>{
-                        let body =res.body;
-                        if(body.errcode == 0){
-                            this.$router.push({name:'questPage'})
-                        }else{
+                    }).then(res => {
+                        let body = res.body;
+                        if (body.errcode == 0) {
+                            this.$router.push({name: 'questPage'})
+                        } else if (body.errcode == 4001) {
+                            this.showNoScroll2 = true;
+                            this.warnText = body.errmsg
+                            setTimeout(function () {
+                                _this.$router.push({
+                                    name: 'quest'
+                                })
+                            }, 500)
+                        } else {
                             this.showNoScroll2 = true;
                             this.warnText = '网络超时，请重试'
                         }
-                    },res=>{
+                    }, res => {
+                        this.showNoScroll2 = true;
+                        this.warnText = '网络超时，请重试'
                     })
-                    this.$router.push({name:'questPage'})
-                }else{
+                    this.$router.push({name: 'questPage'})
+                } else {
                     this.showNoScroll2 = true;
                     this.warnText = '网络超时，请重试'
                 }
@@ -65,20 +76,20 @@
 </script>
 
 <style lang="less" rel="stylesheet/less">
-    .page_quest{
+    .page_quest {
         .vux-header {
             background-color: #AB9236 !important;
         }
         .vux-header .vux-header-title, .vux-header h1 {
             font-size: .85rem;
         }
-        .quest-con{
+        .quest-con {
             width: 100%;
             height: 100%;
             background: url("../assets/ask01.png");
             background-size: 100% 100%;
             position: fixed;
-            .con-logo{
+            .con-logo {
                 position: absolute;
                 top: .7rem;
                 left: .8rem;
@@ -87,16 +98,16 @@
                 background: url("../assets/ask03.png");
                 background-size: 100% 100%;
             }
-            .con-text{
+            .con-text {
                 position: absolute;
                 left: 6.15rem;
                 top: 6.5rem;
                 font-size: .75rem;
-                p{
+                p {
                     line-height: 1.17rem;
                 }
             }
-            .con-integral{
+            .con-integral {
                 color: #FFFFFF;
                 background: #AB9236;
                 margin-left: .2rem;
@@ -107,7 +118,7 @@
                 line-height: 1rem;
                 text-align: center;
             }
-            .con-btn{
+            .con-btn {
                 display: inline-block;
                 position: absolute;
                 left: 9rem;
